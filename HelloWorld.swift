f2efb547eefd46b1ecdfa3d4c7a8f8cfeee47777
Tamer Zreg <tamer.zreg@stu.ocsb.ca>
2024-02-09 15:30:10 +0000

    Added HelloWorld.swift

---
+++ HelloWorld.swift
@@ -0,0 +1,19 @@
+//
+
+//  HelloWorld.swift
+
+//
+
+//  Created by Tamer Zreg
+
+//  Created on 2024-02-08
+
+//  Version 1.0
+
+//  Copyright (c) 2024 Tamer Zreg. All rights reserved.
+
+//
+
+// Outputs Hello World to Console.
+
+print("Hello, World!")
